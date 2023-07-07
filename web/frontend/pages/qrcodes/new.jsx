55574dc76a5beb0844ba576cffed84678ed20b9f
AashirESP <aashirkhan961@gmail.com>
2023-07-07 04:52:19 -0700

    Shopify App Deployment First Commit

---
+++ web/frontend/pages/qrcodes/new.jsx
@@ -0,0 +1,18 @@
+import { QRCodeForm } from "../../components";
+import { Page } from "@shopify/polaris";
+import { TitleBar } from "@shopify/app-bridge-react";
+
+export default function ManageCode() {
+  const breadcrumbs = [{ content: "QR codes", url: "/" }];
+
+  return (
+    <Page>
+      <TitleBar
+        title="Create new QR code"
+        breadcrumbs={breadcrumbs}
+        primaryAction={null}
+      />
+      <QRCodeForm />
+    </Page>
+  );
+}
---
+++ web/shopify.js
@@ -0,0 +1,36 @@
+import sqlite3 from "sqlite3";
+import { join } from "path";
+
+import { QRCodesDB } from "./qr-codes-db.js";
+
+import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
+import { shopifyApp } from "@shopify/shopify-app-express";
+import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
+import { restResources } from "@shopify/shopify-api/rest/admin/2023-04";
+
+const DB_PATH = `${process.cwd()}/database.sqlite`;
+
+// The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
+// See the ensureBilling helper to learn more about billing in this template.
+const database = new sqlite3.Database(join(process.cwd(), "database.sqlite"));
+
+// Initialize SQLite DB
+QRCodesDB.db = database;
+QRCodesDB.init();
+
+const shopify = shopifyApp({
+  api: {
+    apiVersion: LATEST_API_VERSION,
+    restResources,
+    billing: undefined, // or replace with billingConfig above to enable example billing
+  },
+  auth: {
+    path: "/api/auth",
+    callbackPath: "/api/auth/callback",
+  },
+  webhooks: {
+    path: "/api/webhooks",
+  },
+  sessionStorage: new SQLiteSessionStorage(database),
+});
+export default shopify;
